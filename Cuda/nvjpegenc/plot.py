bfa0912c47761743e3ffefcb0d6b313e1f57582e
Lev Veliki <levbo3@yahoo.com>
2024-01-28 14:10:28 +0200

    Add plot from a timingsdata file and make the benchmakr work on gpu without download to cpu and make it work on noised file as well as a non noised one

--- Cuda/nvjpegenc/plot.py
+++ Cuda/nvjpegenc/plot.py
@@ -4,27 +4,27 @@ from glob import glob
 
 fig, ax = plt.subplots()
 
+ax.set_xlabel("qp")
+ax.set_ylabel("fps")
+
 ax.grid()
 
 data = None
 path = "./images/"
-contents = glob(f"{path}*.jpeg")
+contents = None
+qp = []
+fps = []
+skip = 4
+with open("timingdata","r") as f:
+    contents = f.readlines()
+    contents = contents[skip:]
+    for c in contents:
+        c0 = c.split("_")
+        qp.append(float(c0[0]))
+        fps.append(1e6/float(c0[1]))
 
-data = [c.replace(path,"").replace(".jpeg","").split("_")[:] for c in contents]
-data = sorted(data,key= lambda a : float(a[1]))
-print(data)
-
-for i in range(100):
-    fdata = list(filter(lambda a : a[0] == str(i),data))
-    fdata = np.array(fdata,dtype=np.float)
-    if len(fdata.shape) != 2 :
-        print(fdata.shape)
-        continue
-    avg = np.average(fdata[:,1])
-    std = np.std(fdata[:,1])
-    pixpersecond = 1e6 / np.average(fdata[:,1])
-    print(i,pixpersecond, np.abs(pixpersecond - 1e6/(avg + std))  ,np.min(fdata[:,1]),np.max(fdata[:,1]))
-    ax.scatter(fdata[:,2]/1000,1e6 / fdata[:,1])
+#contents = glob(f"{path}*.jpeg")
+ax.plot(qp,fps)
 
 plt.show()
 
--- Cuda/nvjpegenc/test.c
+++ Cuda/nvjpegenc/test.c
@@ -33,21 +33,28 @@ int main(int argc , char ** argv)
     char * inputdata = NULL;
     cudaError_t err = cudaMalloc((void**)&inputdata,w * h * 3);
 
+    char * inputdata0 = NULL;
+    err = cudaMalloc((void**)&inputdata0,w * h * 3);
+
     char * filecontents = NULL;
     guint64 length = 0;
     gboolean filefound = g_file_get_contents ( "FRAME1", &filecontents, &length, NULL);
-    //for (int i = 0 ; i <  640 * 480 * 3 ; i++) 
-    //{
-    //    float sinI = sin(i);
-    //    filecontents[i] = 255 * sin(i);
-    //    if(sinI > 0.4)
-    //    {
-    //        i+=9;
-    //    }
-    //}
     if(filefound && length == w * h * 3)
     {
         cudaMemcpy2D(inputdata ,w *3 ,filecontents, w * 3 ,w * 3 ,h ,cudaMemcpyHostToDevice);
+    }
+    for (int i = 0 ; i <  640 * 480 * 3 ; i++) 
+    {
+        float sinI = sin(i);
+        filecontents[i] = 255 * sin(i);
+        if(sinI > 0.4)
+        {
+            i+=9;
+        }
+    }
+    if(filefound && length == w * h * 3)
+    {
+        cudaMemcpy2D(inputdata0 ,w *3 ,filecontents, w * 3 ,w * 3 ,h ,cudaMemcpyHostToDevice);
     }
 
 #else 
@@ -60,43 +67,60 @@ int main(int argc , char ** argv)
 #endif
 
     
-    nvjpegImage_t nv_image = {};
-    nv_image.channel[0] = inputdata;
-    nv_image.pitch[0] = w * 3;
+    nvjpegImage_t nv_image1 = {};
+    nv_image1.channel[0] = inputdata;
+    nv_image1.pitch[0] = w * 3;
+
+    nvjpegImage_t nv_image0 = {};
+
+    nv_image0.channel[0] = inputdata0;
+    nv_image0.pitch[0] = w * 3;
     // Fill nv_image with image data, let's say 640x480 image in RGB format
 
     guint64 iterations = 100 ;
     guint64 qualityiter = 100; 
 
+
     gint64 starttime = g_get_real_time(); 
-    char * outputdata = NULL;  
+    //char * outputdata = malloc(1000 * 1000 * 2); // ~2MB
+    //
+    char * outputdata =NULL;  
+    err = cudaMalloc((void**)&outputdata, 1000 * 1000 * 2); // ~2MB
+
     g_print(" time %ld\n",starttime);
+    GString * timingsdata = g_string_new("qp_fps_size\n");
     for(int q = 1 ; q < iterations; ++q)
     {
         nvjpegStatus_t jpegsetquality = nvjpegEncoderParamsSetQuality(nv_enc_params,q,stream);
 
         guint64 startencode = g_get_real_time();
-        qualityiter = 100; 
+        qualityiter = 1000; 
         for (int j = 0; j < qualityiter; j++) 
         {
-            nvjpegStatus_t jpegencoded = nvjpegEncodeImage(nv_handle, nv_enc_state, nv_enc_params, &nv_image, NVJPEG_INPUT_RGBI, w, h, stream);
-            //printf("encoded %d \n",jpegencoded);
-            nvjpegEncodeRetrieveBitstream(nv_handle, nv_enc_state, NULL, &length, stream);
-            //g_print("%d length %ld\n",i,length);
-            cudaStreamSynchronize(stream);
+            nvjpegImage_t * nv_image = (j % 2 == 0) ? &nv_image0 : &nv_image1;
 
-            if(outputdata == NULL)
-                outputdata = malloc(length);
+            nvjpegStatus_t jpegencoded = nvjpegEncodeImage(nv_handle, nv_enc_state, nv_enc_params, nv_image, NVJPEG_INPUT_RGBI, w, h, stream);
+            cudaStreamSynchronize(stream);
+            
+            //printf("encoded %d \n",jpegencoded);
+            //To CPU
+            //nvjpegEncodeRetrieveBitstream(nv_handle, nv_enc_state, NULL, &length, stream);
+            //To GPU
+            nvjpegEncodeRetrieveBitstream(nv_handle, nv_enc_state, NULL, &length, stream);
+            //g_print("%d length %ld\n",q,length);
+
             // get stream itself
-            nvjpegEncodeRetrieveBitstream(nv_handle, nv_enc_state, outputdata, &length, 0);
+            nvjpegEncodeRetrieveBitstreamDevice(nv_handle, nv_enc_state, outputdata, &length, 0);
         }
+
         guint64 endencode = g_get_real_time();
         gdouble dt = (gdouble)(endencode - startencode)/(qualityiter);
-        gchar* filename = g_strdup_printf("images/%d_%.1f_%ld.jpeg",q,dt,length);
-        g_file_set_contents(filename,outputdata,length,0);
-        free(outputdata);
-        outputdata = NULL;
+
+        g_string_append_printf(timingsdata,"%d_%.1f_%ld\n",q,dt,length);
+        //g_file_set_contents(filename,outputdata,length,0);
     }
+    
+    g_file_set_contents("timingdata",timingsdata->str,timingsdata->len,0);
     // get compressed stream size
     gint64 endtime= g_get_real_time(); 
     gint64 dtime = endtime-starttime;
